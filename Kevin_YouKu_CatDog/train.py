80f418754f4d7da15d1aff2df6546c8b128bc794
zhangyuchang <990397637@qq.com>
2017-07-06 00:13:02 +0800

    # 用训练好的模型 验证一张图片是猫是狗

--- Kevin_YouKu_CatDog/train.py
+++ Kevin_YouKu_CatDog/train.py
@@ -65,10 +65,72 @@ def run_training():
 
 
 
-run_training()
+# run_training()
+
+
+# 用训练好的模型 验证一张图片是猫是狗
+from  PIL import Image
+import matplotlib.pyplot as plt
+
+# 从训练图片中随机取一张图片
+def get_one_image(train):
 
 
 
+    n = len(train)
+    ind = np.random.randint(0, n)
+    img_dir = train[ind]
+
+    image = Image.open(img_dir)
+    plt.imshow(image)
+    image = image.resize([208, 208])
+    image = np.array(image)
+    return image
+
+
+# 验证
+def evaluate_one_image():
+
+   train_dir = 'data/train/'
+   train, train_label = input_data.get_file(train_dir)
+   image_array = get_one_image(train)
+
+   with tf.Graph().as_default():
+       BATCH_SIZE = 1
+       N_CLASSES = 2
+
+       image = tf.cast(image_array, tf.float32)
+       image = tf.image.per_image_standardization(image)
+       image = tf.reshape(image, [1,208, 208, 3])
+       logit = model.inference(image, BATCH_SIZE, N_CLASSES)
+
+       logit = tf.nn.softmax(logit)
+
+       x = tf.placeholder(tf.float32, [208, 208, 3])
+
+       logs_train_dir = 'logs/train'
+       saver = tf.train.Saver()
+
+       with tf.Session() as sess:
+           print('Reading checkpoints...')
+           ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+           if ckpt and ckpt.model_checkpoint_path:
+              global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+              saver.restore(sess, ckpt.model_checkpoint_path)
+
+              print('loading success, global_step is %s' % global_step)
+           else:
+               print('no checkpoint file found')
+
+           prediction = sess.run(logit, feed_dict={x: image_array})
+           max_index = np.argmax(prediction)
+           if max_index == 0:
+               print('this is a cat with possibility %.6f' %prediction[:,0])
+           else:
+               print('this is a dog with possibility %.6f' %prediction[:,1])
+
+
+evaluate_one_image()
 
 
 
